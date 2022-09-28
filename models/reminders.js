268aa4dbfed8baf21fe01b62db46d4730bbad8ca
Danilo Melo <danilomarquesdemelo@gmail.com>
2022-09-28 18:35:42 -0300

    Criado módulo reminders

---
+++ models/reminders.js
@@ -0,0 +1,23 @@
+module.exports = {
+    reminders: [
+        {
+            id: "id test backend",
+            title: "title test backend",
+            message: "message test backend"
+        }
+    ],
+
+    getAll(){
+        return this.reminders
+    },
+
+    newReminder(title, message){
+        this.reminders.push({id:idGenerator(), title, message})
+    }
+
+}
+
+function idGenerator(){
+    Math.random().toString(36).substring(2, 9)
+}
+
